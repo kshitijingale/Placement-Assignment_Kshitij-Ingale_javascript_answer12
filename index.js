bc1bb9967a7a770169c150a1783ce9394820fdd6
Kshitij Ingale <kshitijingale2@gmail.com>
2023-05-30 20:43:55 +0530

    Fetch data from API and User can also add new blog

---
+++ index.js
@@ -0,0 +1,63 @@
+const postsContainer = document.getElementById('posts-container');
+let blogs = []
+
+// Fetch data from API
+async function getData() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5")
+    const data = await res.json();
+    return data;
+}
+
+// Add fetched data to empty blogs []
+async function addData() {
+    if (blogs.length === 0) {
+        const posts = await getData();
+        await posts.forEach(post => {
+            const blog = {
+                title: post.title,
+                content: post.body
+            }
+            blogs.push(blog)
+        });
+    }
+    return;
+}
+
+// Create Blog
+function createBlog() {
+    const title = document.getElementById('blog-title');
+    const body = document.getElementById('blog-body');
+
+    if (title === "" || body === "") {
+        alert("Fields cannot be empty");
+        return;
+    }
+
+    const blog = {
+        title: title.value,
+        content: body.value
+    }
+    blogs.push(blog);
+    title.value = "";
+    body.value = "";
+    postsContainer.innerHTML = "";
+    showBlogs();
+}
+
+// Show all Blogs on DOM
+async function showBlogs() {
+    await addData();
+    blogs.forEach((blog) => {
+        const post = document.createElement('div')
+        post.setAttribute("class", "post");
+        post.innerHTML = `
+        <h2>${blog.title}</h2>
+        <p>
+          ${blog.content}
+        </p>
+        `
+        postsContainer.appendChild(post);
+    })
+}
+
+showBlogs();
